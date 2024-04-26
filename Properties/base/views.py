76506c2d84c77c675dbca382af9bdd79702e11f3
goncalo <goncalomaioo@gmail.com>
2024-04-26 11:10:50 +0100

    ola

--- Properties/base/views.py
+++ Properties/base/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
-
-def createAccout(request):
-    return render(request, 'sign-up.html')
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # Carrega o perfil criado pelo sinal
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
