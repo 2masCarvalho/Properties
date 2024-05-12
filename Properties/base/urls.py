46eecbfcdd664075ac7603fde667d688f105d75a
Tomas <2mas.silva2003@gmail.com>
2024-05-12 20:00:44 +0100

    Merge remote-tracking branch 'origin/master'

--- Properties/base/urls.py
+++ Properties/base/urls.py
@@ -22,10 +22,9 @@ urlpatterns = [
     path('myproperties/<int:pk>', views.my_properties, name='my_properties'),
     path('property/delete/<int:id>/', views.delete_property_view, name='delete_property'),
     path('property/edit/<int:property_id>/', views.edit_property, name='edit_property'),
-    path('api/myproperties/<int:pk>/', views.my_properties, name='my_properties'),
+
     path('review/<int:host_id>/', views.create_review, name='create_review'),
     path('contactos/', contactos, name='contactos'),
-
 ]
 
 if settings.DEBUG:
